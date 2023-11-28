1f429eab25d523693861b2ec2a37c88eaf256cb8
João Nascimento <nascimento>
2023-11-28 11:01:11 +0200

    empty project

---
+++ concurrency-in-ios-part1/concurrency-in-ios-part1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  concurrency-in-ios-part1
+//
+//  Created by João Barreto do Nascimento on 28.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ concurrency-in-ios-part1/concurrency-in-ios-part1/concurrency_in_ios_part1App.swift
@@ -0,0 +1,17 @@
+//
+//  concurrency_in_ios_part1App.swift
+//  concurrency-in-ios-part1
+//
+//  Created by João Barreto do Nascimento on 28.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct concurrency_in_ios_part1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
